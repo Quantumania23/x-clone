ea153e1dcb347c13808cf485351876230e443e51
Mike Peace <mikepeace981@gmail.com>
2023-09-10 18:36:58 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Sidebar from '../src/Sidebar.js'
+
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hey Mike build a twitter clone </h1>
+
+      {/* Sidebar */}
+      <Sidebar/>
+      
+      {/* Feed */}
+
+      {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from '@mui/icons-material/Home';
+import TagIcon from '@mui/icons-material/Tag';
+
+function Sidebar () {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon/>
+            {/* SidebarOption */} 
+            <HomeIcon/>
+            <TagIcon/>     
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button => Tweet */}
+
+        </div>
+    ) 
+}
+
+export default Sidebar;
